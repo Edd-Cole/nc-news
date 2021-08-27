4aee4720419042f179b398792ea80e17e7b04810
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-08-27 17:07:00 +0100

    Correcting some bugs in Home Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,26 @@
 import { useState, useEffect } from "react";
 import { createNew } from "../utils";
+import { Link } from "react-router-dom";
 
 const Home = () => {
     const [articles, setArticles] = useState([])
 
-    const voteArticle = (event, created_at) => {
+    const voteArticle = (event, article_id) => {
+        console.log(article_id);
         const newArticles = createNew(articles);
-        if(event.target.value = "+") {
+        if(event.target.value === "+") {
+            console.log("+")
             newArticles.map(article => {
-                if(created_at = article.created_at) {
+                if(article_id === article.article_id) {
                     article.votes++;
                     return article;
                 }
                 return article;
             })
-        } else if (event.target.value = "-") {
+        } else if (event.target.value === "-") {
+            console.log("-")
             newArticles.map(article => {
-                if(created_at = article.created_at) {
+                if(article_id === article.article_id) {
                     article.votes--;
                     return article;
                 }
@@ -40,10 +44,12 @@ const Home = () => {
         <section className="homePage">
             <ul>
                 {articles.map(article => {
-                    return <section className="articleHomepage" key={article.created_at}>
+                    const commentLink = `/articles/${article.article_id}/comments`;
+                    const userLink = `/users/${article.author}`
+                    return <section className="articleHomepage" key={article.article_id}>
                         <form className="articleVotesForm" onClick={(event) => {
                             event.preventDefault();
-                            voteArticle(event, article.created_at)}
+                            voteArticle(event, article.article_id)}
                             }>
                             <label>
                             <input type="submit" value="+"/>
@@ -55,6 +61,10 @@ const Home = () => {
                         </form>
                         <section className="articleTitle">
                             <h3>{article.title}</h3>
+                            <p>{article.body.length > 75 ? article.body.slice(0,71) + "..." : article.body}</p>
+                        </section>
+                        <section className="articleInfo">
+                            <p>{article.comment_count} <Link to={commentLink}>Comments</Link> - <Link to={userLink}>{article.author}</Link> - {article.created_at.slice(0,10)}</p>
                         </section>
                     </section>
                 })}
