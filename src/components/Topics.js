35b45278a136f217696d26da081846027fd91ec3
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-09-01 16:25:58 +0100

    Completing logic to fill out the topics page

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const Topics = () => {
+    const [topics, setTopics] = useState([]);
+
+    useEffect(() => {
+        fetch("https://eddncnewsproject.herokuapp.com/api/topics")
+        .then(response => response.json())
+        .then(topics => setTopics(topics.topics))
+    }, [])
+    
     return (
         <section>
             <h2>Topics</h2>
+            <hr></hr>
+            {topics.map(topic => {
+                return (
+                    <section>
+                    <h3><Link to={`/articles?topic=${topic.slug}`}>{topic.slug}</Link></h3>
+                    <p>{topic.description}</p>
+                    <hr></hr>
+                    </section>
+                )
+            })}
         </section>
     )
 }
