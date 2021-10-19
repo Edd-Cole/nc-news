3875d9548b7f3f90caf6d0dda4f2380a908849d8
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-19 10:53:29 +0100

    Adding additional css for nav bar to be responsive

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,10 +3,10 @@ const Nav = ({setPage}) => {
     return (
         <section className="NavBar">
             <ul>
-                <NavLink to="/" activeClassName="currentNav" className="nav" onClick={() => setPage(1)}>Articles</NavLink>
-                <NavLink to="/topics" activeClassName="currentNav" className="nav" onClick={() => setPage(1)}>Topics</NavLink>
-                <NavLink to="/users" activeClassName="currentNav" className="nav" onClick={() => setPage(1)}>Users</NavLink>
-                <NavLink to="/comments" activeClassName="currentNav" className="nav" onClick={() => setPage(1)}>Comments</NavLink>
+                <li><NavLink to="/" activeClassName="currentNav" className="nav" onClick={() => setPage(1)}>Articles</NavLink></li>
+                <li><NavLink to="/topics" activeClassName="currentNav" className="nav" onClick={() => setPage(1)}>Topics</NavLink></li>
+                <li><NavLink to="/users" activeClassName="currentNav" className="nav" onClick={() => setPage(1)}>Users</NavLink></li>
+                <li><NavLink to="/comments" activeClassName="currentNav" className="nav" onClick={() => setPage(1)}>Comments</NavLink></li>
             </ul>
         </section>
     )
